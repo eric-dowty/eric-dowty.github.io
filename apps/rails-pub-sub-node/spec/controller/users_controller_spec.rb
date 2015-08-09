414e1e2fb088b17b48468d172080801691b5c83e
end3rs-army <eric.m.dowty@gmail.com>
2015-08-09 03:26:41 -0600

    added database cleaner to project

---
+++ apps/rails-pub-sub-node/spec/controller/users_controller_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe UsersController, type: :controller do
+
+  describe "POST #create" do
+    it "creates a team" do
+      params = {name: 'original_gangsta'}
+      post :create, format: :json, user: params
+
+      new_user = User.first
+      expect(new_user.name).to eq('original_gangsta')
+    end
+  end
+
+end
